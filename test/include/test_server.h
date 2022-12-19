37207b349a7765759207a230647274319c428e62
Moritz Sokoll <moritz@sokoll.com>
2022-12-19 16:47:01 +0100

    added ipv6 support

--- test/include/test_server.h
+++ test/include/test_server.h
@@ -1,4 +1,5 @@
 #ifndef __HTTPSERV_TEST_SERVER_H__
 #define __HTTPSERV_TEST_SERVER_H__
-void serv_create(void **state);
+void serv_create_v4(void **state);
+void serv_create_v6(void **state);
 #endif
